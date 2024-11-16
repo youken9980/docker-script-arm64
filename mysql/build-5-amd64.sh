699bc66b2ad37e0a4298a602428077ab8090142a
youken9980 <youken9980@163.com>
2024-11-16 15:16:37 +0800

    更新脚本

--- mysql/build-5-amd64.sh
+++ mysql/build-5-amd64.sh
@@ -6,6 +6,6 @@ docker build \
     --build-arg "DEBIAN_MIRROR=${DEBIAN_MIRROR}" \
     --build-arg "KEYSERVER=${KEYSERVER}" \
     --platform linux/amd64 \
-    -f Dockerfile-5 \
+    -f Dockerfile-5-amd64 \
     -t youken9980/mysql:5-debian \
     .
