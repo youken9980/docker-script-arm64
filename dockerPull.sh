cc38231dfcb803fabf3e9ab788106034f367ece2
youken9980 <youken9980@163.com>
2024-11-21 16:17:13 +0800

    更新脚本

--- dockerPull.sh
+++ dockerPull.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-docker pull alpine:3
+docker pull --platform arm64 alpine:3
 
-docker pull centos:7
-docker pull centos:8
+docker pull --platform arm64 centos:7
+docker pull --platform arm64 centos:8
 
-docker pull debian:bookworm
-docker pull debian:bookworm-slim
+docker pull --platform arm64 debian:bookworm
+docker pull --platform arm64 debian:bookworm-slim
 
-docker pull ubuntu:noble
+docker pull --platform arm64 ubuntu:noble
 
-docker pull eclipse-temurin:8-jdk-noble
-docker pull eclipse-temurin:8-jre-noble
-docker pull eclipse-temurin:21-jre-noble
+docker pull --platform arm64 eclipse-temurin:8-jdk-noble
+docker pull --platform arm64 eclipse-temurin:8-jre-noble
+docker pull --platform arm64 eclipse-temurin:21-jre-noble
 
-# docker pull gitlab/gitlab-ce:latest
-docker pull gitlab/gitlab-runner:alpine
+docker pull --platform amd64 gitlab/gitlab-ce:latest
+docker pull --platform arm64 gitlab/gitlab-runner:alpine
 
-docker pull golang:1.12-alpine
+docker pull --platform arm64 golang:1.12-alpine
 
-docker pull jenkins/jenkins:lts-alpine-jdk21
-docker pull jenkins/inbound-agent:alpine-jdk21
-docker pull jenkins/agent:alpine-jdk21
+docker pull --platform arm64 jenkins/jenkins:lts-alpine-jdk21
+docker pull --platform arm64 jenkins/inbound-agent:alpine-jdk21
+docker pull --platform arm64 jenkins/agent:alpine-jdk21
 
-docker pull mysql:8
+docker pull --platform amd64 mysql:5-debian
+docker pull --platform arm64 mysql:8
 
-docker pull nginx:stable-alpine-slim
+docker pull --platform arm64 nginx:stable-alpine-slim
 
-docker pull node:14-alpine
+docker pull --platform arm64 node:14-alpine
 
-docker pull osixia/keepalived:latest
+docker pull --platform amd64 osixia/keepalived:latest
 
-docker pull redis:7-alpine
+docker pull --platform arm64 redis:7-alpine
 
-docker pull registry:2
+docker pull --platform arm64 registry:2
 
-docker pull tomcat:9-jre8-temurin-noble
+docker pull --platform arm64 tomcat:9-jre8-temurin-noble
+
